dc3388c6ea2c8077ea2f45c08f4d715ea0e8b329
Shaibaz07 <sayyedshaibaz336@gmail.com>
2026-09-16 06:41:56 +0530

    update data

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,20 +11,20 @@ const UpdateTodoBtn = document.getElementById('UpdateTodoBtn')
 
 
 
-// let todosArr = [
-//     {
-//         todoItem: "HTML",
-//         todoId: '123'
-//     },
-//     {
-//         todoItem: "CSS",
-//         todoId: '124'
-//     },
-//      {
-//         todoItem: "JavaScript",
-//         todoId: '125'
-//     }
-// ];
+let todosArr = [
+    {
+        todoItem: "HTML",
+        todoId: '123'
+    },
+    {
+        todoItem: "CSS",
+        todoId: '124'
+    },
+     {
+        todoItem: "JavaScript",
+        todoId: '125'
+    }
+];
 
 
 // localStorage.setItem('todosArr' , JSON.stringify(todosArr));
@@ -32,7 +32,7 @@ const UpdateTodoBtn = document.getElementById('UpdateTodoBtn')
 
 
 //API Call to GET todos data
-let todosArr = JSON.parse(localStorage.getItem("todosArr")) || [];
+// let todosArr = JSON.parse(localStorage.getItem("todosArr")) || [];
 cl(todosArr)
 // let todosArr = JSON.parse(todosJson) || []   // JSON to convert JS array
 // cl(todosArr)
